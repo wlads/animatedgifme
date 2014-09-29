31cb581e786535745af74277d899d97a8ac66f48
Wladimir Braguini Domingues <wladimir.braguini@gmail.com>
2014-09-29 01:49:05 -0300

    first version

--- lib/animatedgifme.rb
+++ lib/animatedgifme.rb
@@ -1,5 +1,30 @@
-require "animatedgifme/version"
+require_relative "animatedgifme/version"
+require "httparty"
 
 module Animatedgifme
-  # Your code goes here...
+  include HTTParty
+  base_uri "animatedgif.me"
+
+  def self.find(id)
+  	retrieve_url get("/gifs/#{id}.json")
+  end
+
+  def self.tagged(tag)
+  	retrieve_url get("/#{tag}.json")
+  end
+
+  def self.random(tag)
+  	tagged(tag)
+  end
+
+  private
+  	def self.retrieve_url(response)
+  		response.parsed_response["url"]
+  	end
 end
+
+
+# sample use
+# Animatedgifme.find(1060)
+# Animatedgifme.tagged("cat")
+# Animatedgifme.random("cat") # alias tagged
